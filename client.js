c8fbf2f9ee378d2321a03f5e665a609938689e85
Hadi <h.asiayfard@gmail.com>
2014-08-29 10:08:31 +0430

    make the code cleaner

--- client.js
+++ client.js
@@ -4,29 +4,18 @@ var config=require('./config');
 var serverAddress=config.config.server_address;
 var serverPort=8001;
 var http=require('http');
-
 var url=require('url');
-var currentReq=0;
-
-var number_of_open_connections=0;
 var s=http.createServer(function(req,res){
-    currentReq++;
     var myUrl=req.url;
-    number_of_open_connections+=1;
-    //console.log("Number of open connection is "+number_of_open_connections);
-    console.log('current number of requests(in client): '+currentReq.toString());
     console.log('current Request:');
     console.log(myUrl);
-
     if(myUrl.substr(0,4)!='http')
         throw "error";
-
     var value={
         headers:req.headers,
         url:req.url,
         method:req.method
     };
-    console.log("Request type: "+req.method);
     var str_value=JSON.stringify(value);
     str_value=my_utils.encrypt(str_value);
 
@@ -37,9 +26,7 @@ var s=http.createServer(function(req,res){
         method:req.method
     };
     var getter = http.request(serverOptions, function(res2) {
-            console.log('STATUS: ' + res2.statusCode+" For this url :"+myUrl);
             res.writeHead(res2.statusCode, res2.headers);
-
             res2.pipe(res);
         }
     );
@@ -48,18 +35,14 @@ var s=http.createServer(function(req,res){
     getter.on('error',function()
         {
             console.log("I got an error in client accessing proxy");
-            //console.log("Probably need to end the response");
-            //res.end();
         }
     );
     req.on('error',function()
         {
             console.log("error in client proxy, this shouldn't happened a lot!");
-
         }
     );
     res.on('close',function(){
-        console.log("Connection closed");
         getter.abort();
     });
 
@@ -67,6 +50,6 @@ var s=http.createServer(function(req,res){
 });
 s.setTimeout(30*1000);
 var port=8000;
-console.log("server address is "+serverAddress);
+console.log("Server address is "+serverAddress);
 console.log('listening on port '+port.toString());
 s.listen(port);
--- server.js
+++ server.js
@@ -3,19 +3,9 @@
 my_utils = require('./utils');
 var http = require('http');
 var url = require('url');
-var currentReq = 0;
-var number_of_open_connections = 0;
 var s = http.createServer(function (req, res) {
-    currentReq++;
-    number_of_open_connections++;
-    console.log("Number of open connection is " + number_of_open_connections);
-    console.log('current number of requests( In server): ' + currentReq.toString());
-
     var myUrl = url.parse(req.url, true).query.url;
-
-    //decrypt
     myUrl = my_utils.decrypt(myUrl);
-
     var values = JSON.parse(myUrl);
     var needed_url = values.url;
     var needed_method = values.method;
@@ -32,29 +22,18 @@ var s = http.createServer(function (req, res) {
     };
     console.log('current Request:');
     console.log(needed_url);
-    console.log("Request type: "+needed_method);
-
     var getter = http.request(options, function (res2) {
-        console.log('STATUS: ' + res2.statusCode + " For this url :" + needed_url);
-
         res.writeHead(res2.statusCode, res2.headers);
         res2.pipe(res);
-
-        res2.on('data', function (chunk) {
-                console.log("Still getting data for " + needed_url);
-            }
-        );
     });
 
     req.pipe(getter);
     getter.on('error', function () {
-            console.log("I got an error in server( in amazon, this shouldn't happen a lot");
-            //console.log("I guess I need to end the response!");
-            //res.end();
+            console.log("I got an error in server");
         }
     );
     req.on('error', function () {
-            console.log("error accessing server,( this SHOULD HAPPEN A LOT");
+            console.log("error accessing server");
         }
     );
     res.on('close',function(){
